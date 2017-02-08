173d769271bb79e7831ac60b6c948f1f3e539585
root <root@localhost.localdomain>
2017-02-08 02:46:57 -0500

    removed submodule

--- install_ghost.sh
+++ install_ghost.sh
@@ -3,6 +3,7 @@ cd ghost
 wget https://github.com/TryGhost/Ghost/releases/download/0.11.4/Ghost-0.11.4.zip
 unzip Ghost*.zip
 npm install --production
+rm Ghost*.zip
 cp ../config.js .
 
 # NODE_ENV=production forever node index.js
