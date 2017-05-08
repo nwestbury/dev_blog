cbe12555bd582be0496a9cf8d0795a0a0d5e1f06
Nico <5westbury5@gmail.com>
2017-05-07 22:35:50 -0400

    fixed renew script, contents

--- renew.sh
+++ renew.sh
@@ -3,6 +3,12 @@
 # wget https://dl.eff.org/certbot-auto
 # chmod a+x certbot-auto
 # After running, certbot-auto certonly --standalone -d segfault.ml -d www.segfault.ml
+
+sudo forever stop index.js
+sudo service nginx stop
+
 certbot-auto renew --quiet --no-self-upgrade
 cp /etc/letsencrypt/live/segfault.ml/*.pem /etc/nginx/ssl
-sudo service nginx reload
+
+sudo service nginx start
+NODE_ENV=production forever start /root/dev_blog/index.js
